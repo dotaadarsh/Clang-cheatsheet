ab4cf6aa8db36005a8783d3a904012843f0cc084
Aadarsh K <71810927+madmax-ak@users.noreply.github.com>
2021-12-17 19:18:22 +0530

    Update cheat_sheet.py

--- pages/cheat_sheet.py
+++ pages/cheat_sheet.py
@@ -2,19 +2,21 @@ import streamlit as st
 from streamlit_player import st_player
 
 def app():
-  st.sidebar.title("Contribute")
+  st.sidebar.title("BOOKS")
   st.sidebar.info(
-            "This is an open source project and you are very welcome to contribute your "
-            "comments, questions, resources and apps as "
-            "[issues](https://github.com/giswqs/streamlit-geospatial/issues) or "
-            "[pull requests](https://github.com/giswqs/streamlit-geospatial/pulls) "
-            "to the [source code](https://github.com/giswqs/streamlit-geospatial). ")
+            "[C](https://github.com/EbookFoundation/free-programming-books/blob/main/books/free-programming-books-langs.md#c)"
+            "\n [C++](https://github.com/EbookFoundation/free-programming-books/blob/main/books/free-programming-books-langs.md#c-1)")
+
+  st.sidebar.info(
+            "[Free programming books](https://github.com/EbookFoundation/free-programming-books/blob/main/books/free-programming-books-langs.md)")
 
   st.header("C CHEATSHEET", anchor=None)
   st.caption("REVISE ON THE GO")
-  st_player("https://soundcloud.com/c-programming-podcast/introduction-to-c-programming-language-by-ddsry")
-  with st.expander("KEYWORDS (32)", expanded = False):
-    st.write("""
+  st_player("https://open.spotify.com/show/0lUXb7lZJF4mFW0RE6w2Pc?si=7c5bafa291564167")
+  col1, col2, col3 = st.columns([1,1,1])
+  with col1:
+    with st.expander("KEYWORDS (32)", expanded = True):
+      st.write("""
                 - auto
                 - break 
                 - case
@@ -52,34 +54,10 @@ def app():
                 """,
              unsafe_allow_html=True)
              
-  with st.expander("OPERATORS (45)", expanded=False):
-               st.write("""
-                - Arithmatic 
-                - Relational
-                - Logical
-                - Bitwise
-                - Assignment
-                - Misc
-                <br/><br/>
-                """,
-             unsafe_allow_html=True)
-
-  with st.expander("IDENTIFIERS", expanded=False):
-               st.write("""
-                - An identifier can only have alphanumeric characters (a-z , A-Z , 0-9) (i.e. letters & digits) and underscore( _ ) symbol.
-                - Identifier names must be unique
-                - The first character must be an alphabet or underscore.
-                - You cannot use a keyword as identifiers.
-                - Only the first thirty-one (31) characters are significant.
-                - It must not contain white spaces.
-                - Identifiers are case-sensitive.
-                <br/><br/>
-                """,
-             unsafe_allow_html=True)
-   
-  with st.expander("ESCAPE SEQUENCES (15) - starting with backslash \ ",
-                 expanded=False):
-    st.write("""
+  with col2:
+    with st.expander("ESCAPE SEQUENCES (15) - starting with backslash \ ",
+                 expanded=True):
+                 st.write("""
                 - a - Alarm or Beep
                 - b - Backspace
                 - f - Form Feed
@@ -95,42 +73,23 @@ def app():
                 """,
              unsafe_allow_html=True)
 
-  with st.expander("CONSTANTS - const keyword (or) #define preprocessor",
-                 expanded=False):
-    st.write("""
-                - Decimal Constant - 10, 20, 450 etc.
-                - Real or Floating-point Constant	10.3, 20.2, 450.6 etc.
-                - Octal Constant	021, 033, 046 etc.
-                - Hexadecimal Constant	0x2a, 0x7b, 0xaa etc.
-                - Character Constant	'a', 'b', 'x' etc.
-                - String Constant	"c", "c program", "ZERONITE" etc.
+    with st.expander("OPERATORS (45)", expanded=True):
+      st.write("""
+                - Arithmatic 
+                - Relational
+                - Logical
+                - Bitwise
+                - Assignment
+                - Misc
                 <br/><br/>
                 """,
              unsafe_allow_html=True)
 
-  with st.expander("FORMAT SPECIFIER", expanded=False):
-    st.write("""
-                - **%d or %i** -	It is used to print the signed integer value where signed integer means that the variable can hold both positive and negative values.
-                - **%u**	It is used to print the unsigned integer value where the unsigned integer means that the variable can hold only positive value.
-                - **%o**	It is used to print the octal unsigned integer where octal integer value always starts with a 0 value.
-                - **%x**	It is used to print the hexadecimal unsigned integer where the hexadecimal integer value always starts with a 0x value. In this, alphabetical characters are printed in small letters such as a, b, c, etc.
-                - **%X**	It is used to print the hexadecimal unsigned integer, but %X prints the alphabetical characters in uppercase such as A, B, C, etc.
-                - **%f**	It is used for printing the decimal floating-point values. By default, it prints the 6 values after '.'.
-                - **%e/%E**	It is used for scientific notation. It is also known as Mantissa or Exponent.
-                - **%g**	It is used to print the decimal floating-point values, and it uses the fixed precision, i.e., the value after the decimal in input would be exactly the same as the value in the output.
-                - **%p**	It is used to print the address in a hexadecimal form.
-                - **%c**	It is used to print the unsigned character.
-                - **%s**	It is used to print the strings.
-                - **%ld**	It is used to print the long-signed integer value.
-                <br/><br/>
-                """,
-             unsafe_allow_html=True)
-
-  with st.expander("PRE-PROCESSER DIRECTIVES - starts with hash # ",
-                 expanded=False):
-    st.caption("Syntax: ")
-    st.code('#define token value')
-    st.write("""
+    with st.expander("PRE-PROCESSER DIRECTIVES - starts with hash # ",
+                 expanded=True):
+                 st.caption("Syntax: ")
+                 st.code('#define token value')
+                 st.write("""
                 - #include
                 - #define
                 - #undef
@@ -145,6 +104,50 @@ def app():
                 <br/><br/>
                 """,
              unsafe_allow_html=True)
+
+  with col3:
+    with st.expander("IDENTIFIERS", expanded=True):
+      st.write("""
+                - An identifier can only have alphanumeric characters (a-z , A-Z , 0-9) (i.e. letters & digits) and underscore( _ ) symbol.
+                - Identifier names must be unique
+                - The first character must be an alphabet or underscore.
+                - You cannot use a keyword as identifiers.
+                - Only the first thirty-one (31) characters are significant.
+                - It must not contain white spaces.
+                - Identifiers are case-sensitive.
+                <br/><br/>
+                """,
+             unsafe_allow_html=True)
+    with st.expander("CONSTANTS - const keyword (or) #define preprocessor",
+                 expanded=True):
+                 st.write("""
+                - Decimal Constant - 10, 20, 450 etc.
+                - Real or Floating-point Constant	10.3, 20.2, 450.6 etc.
+                - Octal Constant	021, 033, 046 etc.
+                - Hexadecimal Constant	0x2a, 0x7b, 0xaa etc.
+                - Character Constant	'a', 'b', 'x' etc.
+                - String Constant	"c", "c program", "ZERONITE" etc.
+                <br/><br/>
+                """,
+             unsafe_allow_html=True)
+
+    with st.expander("FORMAT SPECIFIER", expanded=True):
+      st.write("""
+                - **%d or %i** -	signed integer value 
+                - **%u** - unsigned integer value 
+                - **%o** - octal unsigned integer
+                - **%x**/**%X** - hexadecimal unsigned integer 
+                - **%f** - decimal floating-point values
+                - **%e**/**%E** - scientific notation
+                - **%g** - decimal floating-point values, and it uses the fixed precision
+                - **%p** - address in a hexadecimal form
+                - **%c** - unsigned character
+                - **%s** - strings
+                - **%ld** -	long-signed integer value
+                <br/><br/>
+                """,
+             unsafe_allow_html=True)
+
              
   st.code('for i in range(8): foo()')
   code = '''#include <stdio.h>
