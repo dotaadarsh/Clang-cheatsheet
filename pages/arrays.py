c2cfbb972342e747340fbab8d92cb20bd64ca29e
Aadarsh K <71810927+madmax-ak@users.noreply.github.com>
2021-12-17 19:25:55 +0530

    Create arrays.py

---
+++ pages/arrays.py
@@ -0,0 +1,10 @@
+# Load important libraries 
+import streamlit as st 
+from streamlit_player import st_player
+
+def app():
+
+  st.header('ARRAYS')
+  st.caption("An array is defined as the collection of similar type of data items stored at contiguous memory locations. Arrays are the derived data type in C programming language which can store the primitive type of data such as int, char, double, float, etc. It also has the capability to store the collection of derived data types, such as pointers, structure, etc. The array is the simplest data structure where each data element can be randomly accessed by using its index number.")
+
+  st_player("https://www.youtube.com/watch?v=0EgpeYB115s&t=77s")
