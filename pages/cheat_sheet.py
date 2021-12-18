d23ac0b3cd931615c4fa334669271ca2158127f6
Aadarsh K <71810927+madmax-ak@users.noreply.github.com>
2021-12-18 18:43:39 +0530

    Update cheat_sheet.py

--- pages/cheat_sheet.py
+++ pages/cheat_sheet.py
@@ -159,18 +159,3 @@ def app():
    }'''
 
   st.code(code, language='c')
-
-  left_column, right_column = st.columns(2)
- # You can use a column just like st.sidebar:
-  left_column.code('for i in range(8): foo()')
-
- # Or even better, call Streamlit functions inside a "with" block:
-  with right_column:
-    chosen = st.radio('Sorting hat',
-                      ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-  import time
-  my_bar = st.progress(0)
-  for percent_complete in range(100):
-    time.sleep(0.1)
-    my_bar.progress(percent_complete + 1)
