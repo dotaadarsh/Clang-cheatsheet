88d8261b75c2dac2281e189df194c8d434f92110
Aadarsh K <71810927+madmax-ak@users.noreply.github.com>
2021-12-18 21:27:55 +0530

    Update cheat_sheet.py

--- pages/cheat_sheet.py
+++ pages/cheat_sheet.py
@@ -53,6 +53,22 @@ def app():
                 <br/><br/>
                 """,
              unsafe_allow_html=True)
+      with st.expander("LIBRARY FUNCTIONS (11)", expanded = True):
+        st.write("""
+                - <assert.h> - Program assertion functions
+                - <ctype.h>	- Character type functions
+                - <locale.h>	- Localization functions
+                - <math.h>	- Mathematics functions
+                - <setjmp.h>	- Jump functions
+                - <signal.h>	- Signal handling functions
+                - <stdarg.h>	- Variable arguments handling functions
+                - <stdio.h>	- Standard Input/Output functions
+                - <stdlib.h> - Standard Utility functions
+                - <string.h> - String handling functions
+                - <time.h> - Date time functions
+                <br/><br/>
+                """,
+             unsafe_allow_html=True)
              
   with col2:
     with st.expander("ESCAPE SEQUENCES (15) - starting with backslash \ ",
